b8e936fc6408474c67af4d27755e37c59142988d
ty <1550500629@qq.com>
2021-05-12 11:06:42 +0800

    张三第一次提交了代码

---
+++ 01-git/src/com/gec/aa.java
@@ -0,0 +1,13 @@
+package com.gec;
+
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+        System.out.println("1111111111111");
+
+    }
+}
